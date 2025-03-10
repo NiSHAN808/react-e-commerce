d2037261643f7d0f67b867ef9affe2af79bd2b85
NiSHAN808 <nishan4b@gmail.com>
2025-03-10 14:02:33 +0545

    hamburg in navbar done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,45 +1,39 @@
 import "./Navbarstyle.css";
-import React,{useRef} from "react";
-function Navbar(){
-    const pages=useRef(0);
+import React, { useRef, useState } from "react";
+function Navbar() {
+    let pageStyle = useRef(0);
+    const pages = useRef(0);
 
- function   handleHamClick(){
-      console.log("clicked");
-         
-       pages.current.style.height= "100vh";
-        pages.current.justifyContent=" center";
-        pages.current.flexDirection= "column";
-        pages.current.stylePosition= "absolute";
-        pages.current.style.top= "0px";
-        pages.current.style.right= "0px";
-/*    height: 100vh;t
-    justify-content: center;
-    
-    align-items: center;
-    flex-direction: column;
-    position: absolute;
-    top: 0px;
-    right: 0px;  */
+    function handleHamClick() {
+        if (pageStyle.current === 0) {
+
+            pages.current.style.display = "flex";
+            pageStyle.current = 1;
+        }
+        else {
+            pages.current.style.display = "none";
+            pageStyle.current = 0
+        }
     }
-return(
-    <>
-    
-    <nav>
-        <div className="logo">
-            website
-            </div>
-        
-            <div onClick={handleHamClick} className="hamburger">=</div>  
-            <div ref={pages} className="pages">  
-                
-        <a>home</a>
-        <a>products</a>
-        <a>cart</a>
-        </div>
-    </nav>
-    
-    
-    </>
-)
+    return (
+        <>
+
+            <nav>
+                <div className="logo">
+                    website
+                </div>
+
+                <div onClick={handleHamClick} className="hamburger">=</div>
+                <div ref={pages} className="pages">
+
+                    <a>home</a>
+                    <a>products</a>
+                    <a>cart</a>
+                </div>
+            </nav>
+
+
+        </>
+    )
 }
 export default Navbar
