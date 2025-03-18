543a0606f3b36933d3baca61f1fb6e83c2dbcd32
NiSHAN808 <nishan4b@gmail.com>
2025-03-18 15:34:57 +0545

    changed native css to tailwind css

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,14 +3,14 @@ import "./Bannerstyle.css"
 function Banner(){
     
 return(
-    <div className="prosuctCard">
+    <div className="p-1 bg-indigo-100 inline-block ">
         <img src={phoneImage} className="productImages"/>
      <div>
-     <div className="productName">keyboard</div>
-     <div className="productPrice">rs 4000</div>
-     <div className="productSolds">8 sold</div>
+     <div className="bg-teal-200 flex h-10 items-center justify-center">keyboard</div>
+     <div className="bg-purple-300 flex h-10 items-center justify-center">rs 4000</div>
+     <div className="bg-blue-400 flex h-10 items-center justify-center">8 sold</div>
      </div>
-   
+ 
     </div>
 )
 
