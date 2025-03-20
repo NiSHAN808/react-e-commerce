9d5d61aa4f8fdd75615b5d05dca35391cfbd4c12
NiSHAN808 <nishan4b@gmail.com>
2025-03-20 17:29:21 +0545

    little bit of banner design

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -7,7 +7,7 @@ import keyboard from "../images/keyboard.jpg";
 function HomePage(){
 
 
-let datas=[{image:GooglePhone, name:"Google Phone", price:"10,000" ,sold:"120"},
+let datas=[{image:GooglePhone, name:"Google Phone 9 256gb 8gb pink color", price:"10,000" ,sold:"120"},
     {image:tshirt, name:"t-Shirt", price:"2,000" ,sold:"12"},
     {image:iphone, name:"Apple iphoe", price:"100,000" ,sold:"8"},
     {image:keyboard, name:"keyboard", price:"5,000" ,sold:"256"},
@@ -35,7 +35,7 @@ return(
 
     <>
        <div className="w-100vw flex justify-center bg-purple-700">
-        <div className="w-[70rem]  flex justify-center flex-wrap bg-pink-700">
+        <div className="w-[70rem]  flex justify-center flex-wrap bg-gray-200">
         {datas.map((data)=> (<Banner 
         image={data.image} 
         name={data.name}
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,12 +3,12 @@
 function Banner(props){
     
 return(
-    <div className="inline-block h-fit mt-[0.5rem] ml-[0.5rem] inline w-[10rem]">
-        <img src={props.image} className="w-[10rem]"/>
-     <div>
-     <div className="bg-teal-200 flex h-10 items-center justify-center">{props.name}</div>
-     <div className="bg-purple-300 flex h-10 items-center justify-center">rs {props.price}</div>
-     <div className="bg-blue-400 flex h-10 items-center justify-center">{props.sold} sold</div>
+    <div className="inline-block h-fit mt-[0.5rem] ml-[0.5rem] inline w-[10rem] bg-white hover:bg-blue-200 p-[0.3rem]">
+        <img loading="lazy" src={props.image} className="w-[10rem]"/>
+     <div className="pl-[0.4rem]">
+     <div className=" flex h-10">{props.name}</div>
+     <div className=" flex h-7 text-green-500 text-[1.2rem]">rs {props.price}</div>
+     <div className="flex h-10 text-[0.8rem]">{props.sold} sold</div>
      </div>
  
     </div>
