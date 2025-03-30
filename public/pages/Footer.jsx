e440173652ca3413cd9252cb08712583bc9bd3d9
NiSHAN808 <nishan4b@gmail.com>
2025-03-30 16:23:01 +0545

    footer responsive done with deopdown menue

--- public/pages/Footer.jsx
+++ public/pages/Footer.jsx
@@ -1,29 +1,37 @@
+
+import React, { useState } from "react";
+
 function Footer() {
+
+    const [isVisibleSupport, setIsViSupport] = useState(true);   // true == hidden
+    const [isVisibleOther, setIsViOther] = useState(true);
+    const [isVisibleAbout, setIsViAbout] = useState(true);
+
+
+    const toggleSupport = () => {
+        setIsViSupport(!isVisibleSupport);
+    };
+    const toggleOther = () => {
+        setIsViOther(!isVisibleOther);
+    };
+    const toggleAbout = () => {
+        setIsViAbout(!isVisibleAbout);
+    };
     return (
         <footer>
             <div className="inline-block w-full bg-blue-800  text-white inline-block p-[3rem]  ">
                 <div className="flex justify-around max-[700px]:flex-col">
                     <div className="inline-block">
                         <header className="text-[2.5rem] flex justify-between">Support
-                            <button className="hidden max-[700px]:flex">
+                            <button onClick={toggleSupport} className="hidden max-[700px]:flex">{isVisibleSupport ?
                                 <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5H7z" />
+                                </svg> : <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17 14l-5-5-5 5h10z" />
                                 </svg>
-
-
-
-{/* 
-                                <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-  <path d="M17 14l-5-5-5 5h10z"/>
-</svg> */}
-
-
-
-
-
-
+                            }
                             </button>
                         </header>
-                        <ul className="max-[700px]:hidden">
+                        <ul className={isVisibleSupport ? "max-[700px]:hidden" : " "}>
                             <li className="mt-[1.8rem]">Contact us</li>
                             <li className="mt-[1.8rem]">Return policy</li>
                             <li className="mt-[1.8rem]">Terms & conditions</li>
@@ -34,12 +42,15 @@ function Footer() {
 
                     <div className="inline-block">
                         <header className="text-[2.5rem] flex justify-between">Other
-                        <button className="hidden max-[700px]:flex">
+                        <button onClick={toggleOther} className="hidden max-[700px]:flex">{isVisibleOther ?
                                 <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5H7z" />
+                                </svg> : <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17 14l-5-5-5 5h10z" />
                                 </svg>
+                            }
                             </button>
                         </header>
-                        <ul className="max-[700px]:hidden">
+                        <ul className={isVisibleOther ? "max-[700px]:hidden" : " "}>
                             <li className="mt-[1.8rem]">Investors</li>
                             <li className="mt-[1.8rem]">Developer</li>
 
@@ -49,14 +60,17 @@ function Footer() {
 
                     <div className="inline-block">
                         <header className="text-[2.5rem] flex justify-between">About
-                        <button className="hidden max-[700px]:flex">
+                        <button onClick={toggleAbout} className="hidden max-[700px]:flex">{isVisibleAbout ?
                                 <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5H7z" />
+                                </svg> : <svg width="55" height="55" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17 14l-5-5-5 5h10z" />
                                 </svg>
+                            }
                             </button>
 
 
                         </header>
-                        <ul className="max-[700px]:hidden">
+                        <ul className={isVisibleAbout ? "max-[700px]:hidden" : " "}>
                             <li className="mt-[1.8rem]">Carrer</li>
                             <li className="mt-[1.8rem]">Refer a friend</li>
                             <li className="mt-[1.8rem]">Terms & conditions</li>
