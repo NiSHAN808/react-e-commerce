fcb4b1478e8ca38c93220dd2e8a2c25f71802c5f
NiSHAN808 <nishan4b@gmail.com>
2025-05-25 17:55:27 +0545

    made cart page for phone and pc

--- public/pages/CartPage.jsx
+++ public/pages/CartPage.jsx
@@ -25,8 +25,8 @@ function handleDelClick(){
     }
     return (
 
-        <div className="bg-white inline-flex w-[80vw] p-[0.5rem] w-[10rem] mt-[1rem]"><div className=" flex flex-col justify-around">
-            <input className="h-[2rem] w-[2rem] mr-[3rem]" onClick={cartItemSeletec} type="checkbox"></input>
+        <div className="bg-white inline-flex w-[100vw] md:w-[80vw] p-[0.5rem] w-[10rem] mt-[1rem]"><div className=" flex flex-col justify-around">
+            <input className="h-[1.5rem] w-[1.5rem] md:h-[2rem] md:w-[2rem]  mr-[1.5rem] md:mr-[3rem]" onClick={cartItemSeletec} type="checkbox"></input>
             <button onClick={handleDelClick}> 
             
 <svg
@@ -67,13 +67,14 @@ function handleDelClick(){
 </svg>
                 </button></div>
             <img src={imageLocation} className="w-[10vw] h-[10vw] inline-block" />
-            <div className="inline-block w-[42%] ml-[2rem]">
-                <h3 > {props.name}</h3>
+            <div  className="w-[full] inline-flex flex-col md:flex-row ">
+            <div className="inline-block w-[full] lg:w-[60%] ml-[2rem] mb-[1rem]">
+                <h3 className="text-[3vw] lg:text-[2vw] xl:text-[1.5vw]"> {props.name}</h3>
                 <h3 className="text-[1.5rem] text-blue-800"> $ {props.price}</h3>
 
             </div>
-            <div className="inline-flex h-auto  flex-col  justify-center items-center  ml-[2rem]">
-                <h3 className="text-[1.6rem] font-serif"> color :  {props.color}</h3>
+            <div className="inline-flex h-auto  flex-row md:flex-col  justify-around items-center  ml-[2rem]">
+                <h3 className="text-[1rem]  md:text-[1.6rem] font-serif"> color :  {props.color}</h3>
                 <div className="flex">
                         <button className="bg-blue-100   h-[2.3rem] w-[2.3rem]" onClick={handleQuantityDecrease} >-</button>
                         <div contenteditable="true" className="bg-blue-200 inline-flex  h-[2.3rem] w-[2.3rem]  justify-center items-center" >{quantity}</div>
@@ -81,12 +82,43 @@ function handleDelClick(){
                     </div>
 
             </div>
+
+            </div>
             <div onClick={(e)=>cartItemSeletec(e)} className="inline-block"></div>
         </div>
     )
 }
 
+function NoItemInTheCart(){
+    return(
+        <div className="h-[100vh] inline-flex justify-center items-center">
+        
+        <div className="flex justify-center items-center">
+<div className=" w-[95vw] md:w-[90vw] flex justify-around items-center">
+<div> <svg width="20vw" height="1" xmlns="http://www.w3.org/2000/svg">
+  <line x1="0" y1="0" x2="20vw" y2="0" stroke="#888" stroke-width="6" />
+</svg>
+</div>
 
+<div className=" h-[10vw]  md:h-[7vw] lg:h-[5vw]  text-[5vw] md:text-[3vw] lg:text-[2vw] xl:text-[2vw] ml-[3vw] mr-[3vw] font-mono inline-flex justify-center">
+No Items In Cart
+</div>
+
+<div className=""><svg width="20vw" height="1" xmlns="http://www.w3.org/2000/svg">
+  <line x1="0" y1="0" x2="20vw" y2="0" stroke="#888" stroke-width="6" />
+</svg></div>
+</div>
+
+
+
+</div>
+        
+        
+        
+        
+        </div>
+    )
+}
 function CartPage() {
 
     const [billPrice, setBillPrice] = useState(0);
@@ -100,7 +132,7 @@ setCartList(JSON.parse(localStorage.getItem("cartItem")));
         <div className=" flex flex-col  items-center bg-gray-200 w-full">
 
 
-            {cartList.length===0 ?<>no data availabe</> : cartList.map((elm,index) => <Item
+            {cartList===null ?<><NoItemInTheCart/></> : cartList.map((elm,index) => <Item
                 billPrice={billPrice}
                 setBillPrice={setBillPrice}
                 image={elm.image}
